bb3f98f4ab9e47c71ef0b7571a3382f47afd98bd
Erik <erik.hultqvist@gritacademy.se>
2026-01-14 09:34:09 +0100

    Homework 3, plus homework 1 corrections

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,18 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
 import { Movie } from '../../model/movie.type';
+import { MovieService } from '../../services/movie.service';
+import { DurationPipe } from '../../pipes/duration-pipe';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [],
+  imports: [ DurationPipe ],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.scss',
 })
 export class MovieCardComponent {
   @Input({ required: true }) movie!: Movie;
-
+  
   @Output() addToFavourites = new EventEmitter<Movie>();
   @Output() addToWatchList = new EventEmitter<Movie>();
+
+  showDetails = signal(false);
+  runtime = signal<number | null>(null);
+  loading = signal(false);
+
+  constructor(private movieService: MovieService) {}
 
   onFavouriteClick() {
     this.addToFavourites.emit(this.movie);
@@ -20,5 +28,25 @@ export class MovieCardComponent {
 
   onWatchListClick() {
     this.addToWatchList.emit(this.movie);
+  }
+
+  showMoreDetailsClick() {
+    if (this.showDetails()) {
+      this.showDetails.set(false);
+      return;
+    }
+
+    if (this.runtime()) {
+      this.showDetails.set(true);
+      return;
+    }
+
+    this.loading.set(true);
+
+    this.movieService.getMovieDetails(this.movie.id).subscribe(details => {
+      this.runtime.set(details.runtime);
+      this.showDetails.set(true);
+      this.loading.set(false);
+    });
   }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -36,56 +36,13 @@ export class MovieService {
         description: apiItem.overview,
       };
   }
+
+  getMovieDetails(movieId: number): Observable<{ runtime: number }> {
+  const env = environment as { apiUrl: string; apiKey: string };
+
+  return this.http.get<{ runtime: number }>(
+    `${env.apiUrl}/movie/${movieId}?api_key=${env.apiKey}`
+  );
 }
 
-
-
-
-
-
-
-
-/* // src/app/services/movie.service.ts
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-
-import { MovieApiItem, MovieApiResponse } from '../model/movie-api.model';
-import { Movie } from '../model/movie.model';
-import { environment } from '../../environments/environment';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class MovieService {
-  constructor(private http: HttpClient) {}
-
-  getMoviesFromApi(): Observable<Movie[]> {
-    const env = environment as { apiUrl: string; apiKey: string };
-
-    return this.http
-      .get<MovieApiResponse>(
-        `${env.apiUrl}/movie/popular?api_key=${env.apiKey}`
-      )
-      .pipe(
-        map(response =>
-          response.results.map(this.mapApiToMovie)
-        )
-      );
-  }
-
-  private mapApiToMovie(apiItem: MovieApiItem): Movie {
-    return {
-      id: apiItem.id,
-      poster: apiItem.poster_path,
-      title: apiItem.title,
-      rating: apiItem.vote_average,
-      year: new Date(apiItem.release_date).getFullYear(),
-      genres: apiItem.genre_ids,
-      description: apiItem.overview,
-      director: undefined, // TMDB requires separate request for crew
-    };
-  }
 }
- */
